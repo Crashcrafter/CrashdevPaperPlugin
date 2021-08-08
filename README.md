9e724b8162093ae54a73cba77ced27132ac0755e
Crashcrafter <n.hack2810@gmail.com>
2021-08-08 15:22:12 +0200

    added example config, removed unused data class

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# CrashdevPaperPlugin
+# Disclaimer:
+
+There is no guarantee that the plugin works on every server! Im not responsible for any damage caused by this plugin or its bugs!
+
+#Contact:
+Discord: https://discord.gg/qQtaYsDN6w
--- src/main/kotlin/dev/crash/JsonObjects.kt
+++ src/main/kotlin/dev/crash/JsonObjects.kt
@@ -1,12 +1,5 @@
 package dev.crash
 
-data class SpawnObj(
-    val x: Double,
-    val y: Double,
-    val z: Double,
-    val world: String
-)
-
 class CoingeckoPriceInfo(elements: Map<String, CoingeckoCurrencyValue>) : HashMap<String, CoingeckoCurrencyValue>(elements)
 
 data class CoingeckoCurrencyValue (
